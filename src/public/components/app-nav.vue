88f992ecaf222f9f37a23a156d6d8b6e782cdb84
multios12 <multiple12@gmail.com>
2018-08-11 08:13:37 +0900

    fix: navbarのレイアウト変更

--- src/public/components/app-nav.vue
+++ src/public/components/app-nav.vue
@@ -7,24 +7,26 @@
         <b-form-select v-model="selectedMonth" :options="months" @change="changeMonth" class="mr-1" />
       </b-navbar-nav>
         <b-nav-text class="mr-1"> 残高{{balance}}</b-nav-text>
-    <b-navbar-nav is-nav class="mr-1 float-left">
-      <router-link to="/saving" class="btn btn-secondary"><i class="fas fa-piggy-bank"></i></router-link>
-    </b-navbar-nav>
-    <b-navbar-nav is-nav class="mr-1 float-left">
-      <router-link to="/transfer" class="btn btn-secondary mr-1"><i class="fas fa-exchange-alt"></i></router-link>
-    </b-navbar-nav>
-    <b-navbar-nav is-nav class="float-left">
-      <router-link to="/today" class="btn btn-secondary mr-1">today</router-link>
-    </b-navbar-nav>
+      <b-navbar-nav is-nav class="mr-1 float-left">
+        <router-link to="/saving" class="btn btn-secondary"><i class="fas fa-piggy-bank"></i></router-link>
+      </b-navbar-nav>
+      <b-navbar-nav is-nav class="mr-1 float-left">
+        <router-link to="/transfer" class="btn btn-secondary mr-1"><i class="fas fa-exchange-alt"></i></router-link>
+      </b-navbar-nav>
+      <b-navbar-nav is-nav class="float-left" right>
+        <router-link to="/today" class="btn btn-secondary mr-1">today</router-link>
+      </b-navbar-nav>
 
-    <b-nav-item-dropdown right>
-      <!-- Using button-content slot -->
-      <template slot="button-content">
-        <i class="fas fa-user"></i>
-      </template>
-      <b-dropdown-item href="/#/settings"><i class="fas fa-cog"></i>settings</b-dropdown-item>
-      <b-dropdown-item href="/#/logout"><i class="fas fa-sign-out-alt"></i>signout</b-dropdown-item>
-    </b-nav-item-dropdown>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item-dropdown>
+        <!-- Using button-content slot -->
+        <template slot="button-content">
+          <i class="fas fa-user"></i>
+        </template>
+        <b-dropdown-item href="#/settings"><i class="fas fa-cog"></i>settings</b-dropdown-item>
+        <b-dropdown-item href="#/logout"><i class="fas fa-sign-out-alt"></i>signout</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
     </b-collapse>
   </b-navbar>
 </template>
